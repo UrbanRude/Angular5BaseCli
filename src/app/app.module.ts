b4f7312a96927f9db1ed886b050cd9fb30339b92
UrbanRude <urban.rude@gmail.com>
2018-04-20 13:39:34 -0500

    Add Select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { PasivoComponent } from './pasivo/pasivo.component';
 import { ReactivoComponent } from './reactivo/reactivo.component';
 import { LoginComponent } from './login/login.component';
 import { LoginComponentCard } from './login-card/login-card.component';
+import { MatSelectModule} from '@angular/material/select';
 
 export const MATERIAL_COMPONENTS = [
   MatToolbarModule,
@@ -32,7 +33,8 @@ export const MATERIAL_COMPONENTS = [
   MatListModule,
   MatIconModule,
   MatInputModule,
-  MatProgressBarModule
+  MatProgressBarModule,
+  MatSelectModule
 ]
 
 @NgModule({
